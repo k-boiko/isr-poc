cce1d3c9bf85c412fddfed45493d2d8a558b31ee
k-boiko <kboyko1999@gmail.com>
2025-05-21 22:17:36 +0100

    log all incoming params to a function

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,7 +5,9 @@ export async function generateStaticParams() {
   return []
 }
 
-export default async function Product({ params }: { params: Promise<{id: string}>}) {
+export default async function Product(ars: { params: Promise<{id: string}>}) {
+  const { params } = ars;
+  console.log(ars);
   const {id} = await params;
   const {host} = await (await fetch('https://isr-poc-alpha.vercel.app/api/host')).json();
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
