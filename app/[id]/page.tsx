6e6393227c50c99d06f7f21580bb29517339db79
k-boiko <kboyko1999@gmail.com>
2025-05-21 17:32:41 +0100

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -2,7 +2,7 @@ export const revalidate = 600
 
 export default async function Product({ params }: { params: Promise<{id: string}>}) {
   const {id} = await params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/product/${id}`)
+  const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}`)
   const data = await res.json()
   return <div>{JSON.stringify(data)}
   </div>
