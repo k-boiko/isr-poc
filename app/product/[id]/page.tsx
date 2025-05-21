8713c8c088d5b0ea10c7968f6e5311d2c49fd354
k-boiko <kboyko1999@gmail.com>
2025-05-21 18:51:48 +0100

    fix api url

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -7,7 +7,7 @@ export async function generateStaticParams() {
 
 export default async function Product({ params }: { params: Promise<{id: string}>}) {
   const {id} = await params;
-  const host = await (await fetch('/api/host')).json();
+  const host = await (await fetch('https://isr-poc-alpha.vercel.app/api/host')).json();
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
   const data = await res.json();
   return <div>{JSON.stringify(data)}
