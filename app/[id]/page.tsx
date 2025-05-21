0cdbb52d525ac1e062a568b69601f3cc528a89e5
k-boiko <kboyko1999@gmail.com>
2025-05-21 17:29:04 +0100

    Update page.tsx

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,6 +1,6 @@
 export const revalidate = 600
 
-export default async function Product({ params }: Promise<{id: string}>) {
+export default async function Product({ params }: { params: Promise<{id: string}>}) {
   const {id} = await params;
   const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/product/${id}`)
   const data = await res.json()
