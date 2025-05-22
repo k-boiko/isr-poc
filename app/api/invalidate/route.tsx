689e2cb8a90f9b8ddde61b4e9cdf594104172ef0
k-boiko <kboyko1999@gmail.com>
2025-05-22 09:54:31 +0100

    change revalidate function

--- app/api/invalidate/route.tsx
+++ app/api/invalidate/route.tsx
@@ -1,4 +1,7 @@
+'use server';
+
 import { NextRequest, NextResponse } from "next/server";
+import { revalidatePath } from 'next/cache';
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
@@ -6,7 +9,7 @@ export async function GET(request: NextRequest) {
   console.log({path});
   try {
     // @ts-ignore
-    await (NextResponse as any).revalidate(path);
+    await revalidatePath(path);
     return NextResponse.redirect(new URL(path, request.url))
   } catch (err) {
     console.log(err);
