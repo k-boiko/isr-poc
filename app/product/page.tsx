a1da9cacc063c9db0318aa9ef930d5d0c258f306
k-boiko <kboyko1999@gmail.com>
2025-05-22 16:40:28 +0100

    verify getting host name on page without params

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,7 +1,17 @@
 export const revalidate = 600;
 
-export default async function Page() {
-  const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/1`);
+export default async function Page(context) {
+  const {params} = context;
+  console.log({context});
+  console.log({params});
+  const symbols = Object.getOwnPropertySymbols(params);
+  const kstoresymbol = symbols.find(s => s.toString().includes('kResourceStore'));
+  let host = 'host';
+  if (kstoresymbol) {
+    const store = (params as any)[kstoresymbol];
+    host = store.url.host;
+  }
+  const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/1?host=${host}`);
   const data = await res.json();
 
   return (
