c68bde8d64a3b58ff37156d5bdc9599ded55fb68
k-boiko <kboyko1999@gmail.com>
2025-05-22 09:18:09 +0100

    use resolved params object to get info

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -7,17 +7,18 @@ export async function generateStaticParams() {
 
 export default async function Product(ars: { params: Promise<{id: string}>}, ...ars2: any[]) {
   const { params } = ars;
+  const paramsResolved = await params;
   console.log({ars});
-  const symbols = Object.getOwnPropertySymbols(ars);
+  const symbols = Object.getOwnPropertySymbols(paramsResolved);
   const kstoresymbol = symbols.find(s => s.toString().includes('kResourceStore'));
   console.log({kstoresymbol});
   console.log({symbols});
   if (kstoresymbol) {
-    const store = (ars as any)[kstoresymbol];
+    const store = (paramsResolved as any)[kstoresymbol];
     console.log({store});
   }
   console.log({ars2});
-  const {id} = await params;
+  const {id} = paramsResolved;
   const {host} = await (await fetch('https://isr-poc-alpha.vercel.app/api/host')).json();
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
   const data = await res.json();
