81f20acd6073895fe888049cda9f9786a6bd4ba0
k-boiko <kboyko1999@gmail.com>
2025-05-22 09:08:23 +0100

    more debug

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,9 +5,17 @@ export async function generateStaticParams() {
   return []
 }
 
-export default async function Product(ars: { params: Promise<{id: string}>}) {
+export default async function Product(ars: { params: Promise<{id: string}>}, ...ars2: any[]) {
   const { params } = ars;
-  console.log(ars);
+  console.log({ars});
+  const symbols = Object.getOwnPropertySymbols(ars);
+  const kstoresymbol = symbols.find(s => s.toString().includes('kResourceStore'));
+  console.log({kstoresymbol});
+  if (kstoresymbol) {
+    const store = ars[kstoresymbol];
+    console.log({store});
+  }
+  console.log({ars2});
   const {id} = await params;
   const {host} = await (await fetch('https://isr-poc-alpha.vercel.app/api/host')).json();
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
