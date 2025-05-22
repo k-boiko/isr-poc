4be13e61eeb183c2808d42266d15da2b87860e68
k-boiko <kboyko1999@gmail.com>
2025-05-22 10:07:47 +0100

    remove prefetch??

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -8,23 +8,19 @@ export async function generateStaticParams() {
 }
 
 export default async function Product({ params }: { params: Promise<{id: string}>}) {
-  const paramsResolved = await params;
-  console.log(paramsResolved);
-  const symbolsResolved = Object.getOwnPropertySymbols(paramsResolved);
   const symbols = Object.getOwnPropertySymbols(params);
   const kstoresymbol = symbols.find(s => s.toString().includes('kResourceStore'));
-  const kstoresymbolResolvied = symbolsResolved.find(s => s.toString().includes('kResourceStore'));
   let host = 'host';
   if (kstoresymbol) {
     const store = (params as any)[kstoresymbol];
     host = store.url.host;
   }
-  const {id} = paramsResolved;
+  const {id} = await params;
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
   const data = await res.json();
 
   return <div>
     <div>{JSON.stringify(data)}</div>
-    <div><Link href={`/api/invalidate?path=${encodeURIComponent(`/product/${id}`)}`}>invalidate cache of this page</Link></div>
+    <div><Link href={`/api/invalidate?path=${encodeURIComponent(`/product/${id}`)}`} prefetch={false}>invalidate cache of this page</Link></div>
   </div>
 }
