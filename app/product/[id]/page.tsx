f18cdcb081af6e5af306d1f443938f2f4ebfba26
k-boiko <kboyko1999@gmail.com>
2025-05-22 09:24:08 +0100

    more logs

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -5,19 +5,21 @@ export async function generateStaticParams() {
   return []
 }
 
-export default async function Product(ars: { params: Promise<{id: string}>}, ...ars2: any[]) {
+export default async function Product({ params }: { params: Promise<{id: string}>}) {
   const { params } = ars;
   const paramsResolved = await params;
-  console.log({ars});
-  const symbols = Object.getOwnPropertySymbols(paramsResolved);
+  console.log(paramsResolved);
+  const symbolsResolved = Object.getOwnPropertySymbols(paramsResolved);
+  const symbols = Object.getOwnPropertySymbols(params);
   const kstoresymbol = symbols.find(s => s.toString().includes('kResourceStore'));
-  console.log({kstoresymbol});
-  console.log({symbols});
+  const kstoresymbolResolvied = symbolsResolved.find(s => s.toString().includes('kResourceStore'));
+  console.log({kstoresymbol, kstoresymbolResolvied});
+  console.log({symbols, l: symbols.length});
+  console.log({symbolsResolved, l: symbolsResolved.length});
   if (kstoresymbol) {
     const store = (paramsResolved as any)[kstoresymbol];
     console.log({store});
   }
-  console.log({ars2});
   const {id} = paramsResolved;
   const {host} = await (await fetch('https://isr-poc-alpha.vercel.app/api/host')).json();
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/${id}?host=${host}`);
