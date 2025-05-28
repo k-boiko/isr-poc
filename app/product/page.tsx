12e55ae0ac96636ae1901090392d7921a5013a60
k-boiko <kboyko1999@gmail.com>
2025-05-28 13:09:32 +0100

    test Intl

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -9,17 +9,20 @@ export default async function Page(context: any) {
   let host = 'host';
   if (kstoresymbol) {
     const store = (params as any)[kstoresymbol];
-    console.log(store);
     host = store?.url?.host;
   }
   const res = await fetch(`https://b6e33301-2517-4d30-ae96-98e9a71a7f0d-00-1tghupfuenc4c.kirk.replit.dev/api/product/1?host=${host}`);
   const data = await res.json();
+  const a = {
+    d1: new Intl.DisplayNames(`de`, { type: 'language'}).of('gsw') ,
+    d: new Intl.DisplayNames(`deu`, { type: 'language'}).of('gsw') ,
+    s: new Intl.DisplayNames(`gsw`, { type: 'language'}).of('gsw') ,
+    e: new Intl.DisplayNames(`en`, { type: 'language'}).of('gsw') 
+  };
 
   return (
     <div>
-      <h1>{data.msg}</h1>
-      <div>Domain: {data.domain}</div>
-      <div>Time: {data.time}</div>
+      <div>{JSON.stringify(a)}</div>
     </div>
   );
 }
