b950121893bc274fd42b606de164dc661ac11d21
k-boiko <kboyko1999@gmail.com>
2025-06-27 11:16:19 +0100

    add dynamic api usage for webmanifest

--- app/manifest.ts
+++ app/manifest.ts
@@ -1,4 +1,10 @@
+import { headers } from 'next/headers';
 export default async function manifest(context: any) {
+    const host = (await headers()).get('host');
+    console.log({host, context});
+    if (!host) {
+        return {};
+    }
     console.log({context});
     return {};
 }
